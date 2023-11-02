a3973916e518926669acffe60073f92bd72f06f8
Hanifi Demir <101357112+hanifidemir5@users.noreply.github.com>
2023-11-02 22:45:16 +0300

    commit

--- KargoApp/Program.cs
+++ KargoApp/Program.cs
@@ -1,4 +1,6 @@
 using KargoApp.Data;
+using KargoApp.Interface;
+using KargoApp.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IOrderRepository, OrdersRepository>();
+builder.Services.AddScoped<ICarriersRepository, CarriersRepository>();
+builder.Services.AddScoped<ICarrierConfigurationsRepository, CarrierConfigurationsRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
